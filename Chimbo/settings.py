f0e8d2292cd703aba9c1fd8e642c383fbad84284
Cgboal <cgboal521@gmail.com>
2016-02-07 14:34:01 +0000

    stuff

--- Chimbo/settings.py
+++ Chimbo/settings.py
@@ -78,9 +78,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = 'Chimbo/chApp/static/'
-
+STATIC_URL = os.path.join(BASE_DIR) + '/static/'
+print STATIC_URL
+STATIC_ROOT = os.path.join(BASE_DIR) + '/static/'
+MEDIA_ROOT = STATIC_URL + '/media/'
 TEMPLATE_DIRS = (
-	'chApp/templates/',
+    os.path.join(BASE_DIR) + '/chApp/templates/',
 )

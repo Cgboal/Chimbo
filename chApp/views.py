2ee6899e02dd2731e9d37eb9abb6c02e665785dc
Cgboal <cgboal521@gmail.com>
2016-02-09 17:53:16 +0000

    Pushing testing etc

--- chApp/views.py
+++ chApp/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 # Create your views here.
 
+
 def index(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
+
+
+def welcome(request):
+    return render_to_response('welcome.html', context_instance=RequestContext(request))
+
+
+def login(request):
+    return render_to_response('login.html', context_instance=RequestContext(request))
+
+
+def navTest(request):
+    return render_to_response('navtest.html', context_instance=RequestContext(request))

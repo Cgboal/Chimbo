978eadd972776d0a215c1793b05f82bf60924c87
Cgboal <cgboal521@gmail.com>
2016-04-24 22:31:14 +0100

    register test

--- chApp/models.py
+++ chApp/models.py
@@ -3,16 +3,14 @@ from django.db import models
 # Create your models here.
 
 
-class Test(models.Model):
-    test_field = models.CharField(max_length=200)
+class User(models.Model):
+    userName = models.CharField(max_length=20)
+    pHash = models.CharField(max_length=32)
+    perms = models.PositiveSmallIntegerField()
+    fName = models.CharField(max_length=20)
+    lName = models.CharField(max_length=20)
+    country = models.CharField(max_length=30)
+    email = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.test_field
-
-
-class User(models.Model):
-    Fname = models.CharField(max_length=200)
-    Lname = models.CharField(max_length=200)
-    Country = models.CharField(max_length=200)
-    Email = models.CharField(max_length=200)
-    Password = models.CharField(max_length=32)
+        return self.userName
--- chApp/views.py
+++ chApp/views.py
@@ -1,23 +1,33 @@
-from django.shortcuts import render_to_response
+import hashlib
+from django.shortcuts import render
 from django.template.context import RequestContext
+from .models import User
 # Create your views here.
 
 
 def index(request):
-    return render_to_response('index.html', context_instance=RequestContext(request))
+    return render('index.html', context_instance=RequestContext(request))
 
 
 def welcome(request):
-    return render_to_response('welcome.html', context_instance=RequestContext(request))
+    return render('welcome.html', context_instance=RequestContext(request))
 
 
 def login(request):
-    return render_to_response('login.html', context_instance=RequestContext(request))
+    return render('login.html', context_instance=RequestContext(request))
 
 
 def navTest(request):
-    return render_to_response('navtest.html', context_instance=RequestContext(request))
+    return render('navtest.html', context_instance=RequestContext(request))
 
 
 def signUp(request):
-    return render_to_response('signup.html', context_instance=RequestContext(request))
+    req = ['username', 'fName', 'lName', 'country', 'email', 'pass', 'rePass']
+    for i in req:
+        if i not in request.POST:
+            return render('signup.html', context_instance=RequestContext(request))
+    h = hashlib.md5(request.POST['pass']).hexdigest()
+    u = User(userName=request.POST['username'], pHash=h, perms=0, fName=request.POST['fName'],
+             lName=request.POST['lName'], country=request.POST['country'], email=request.POST['email'])
+    u.save()
+    return render(request, 'index.html')

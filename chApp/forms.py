c5b43da6f827900abc58f043f61b879663a6c5e4
cgboal <cgboal521@gmail.com>
2016-05-11 00:46:53 +0100

    Again again

--- chApp/forms.py
+++ chApp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django_countries import countries
 from django_countries.fields import LazyTypedChoiceField
 
 
@@ -7,5 +8,5 @@ class regForm(forms.Form):
     userName = forms.CharField(label='Username', max_length=20)
     fName = forms.CharField(label='First Name', max_length=20)
     lName = forms.CharField(label='Last Name', max_length=20)
-    country = LazyTypedChoiceField(label='Country', choices='countries')
+    country = LazyTypedChoiceField(label='Country', choices=countries)
     email = forms.EmailField(label='Email', max_length=100)

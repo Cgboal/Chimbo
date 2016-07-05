5ac930f8b9263d42ead41a418b0489efa84fcb33
root <root@localhost.localdomain>
2016-07-05 10:46:22 -0400

    Added models

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
+
+admin.site.register(models.Course)
+admin.site.register(models.Module)
+admin.site.register(models.Note)
--- notes/models.py
+++ notes/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=80)
+    years = models.IntegerField()
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    year = models.IntegerField()
+
+class Note(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    title = models.TextField()
+    body = models.TextField()
+

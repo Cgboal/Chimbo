37560b8da5937d6bdc228bf3c70ea264e71f9ba1
root <root@localhost.localdomain>
2016-07-05 10:57:21 -0400

    Added str methods to models

--- notes/models.py
+++ notes/models.py
@@ -8,13 +8,23 @@ class Course(models.Model):
     name = models.CharField(max_length=80)
     years = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Module(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
     year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
 
 class Note(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
     title = models.TextField()
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
 

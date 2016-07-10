14563d8a05f9551a3faafe8b7ccd8eca65da3d2e
root <root@localhost.localdomain>
2016-07-09 20:00:56 -0400

    Changed models

--- notes/models.py
+++ notes/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Course(models.Model):
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, primary_key=True)
     years = models.IntegerField()
 
     def __str__(self):

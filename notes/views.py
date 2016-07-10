cd2a57cfe5da435b13a6c9c5cf95e63653770e05
root <root@localhost.localdomain>
2016-07-10 14:03:27 -0400

    Added note viewing

--- notes/views.py
+++ notes/views.py
@@ -1,28 +1,47 @@
 from django.shortcuts import render
 import models
 from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 
 @login_required()
 def dashboard(request):
     return render(request, 'base.html')
 
+
 @login_required()
 def courseList(request):
     c = models.Course.objects.all()
-    return render(request, 'courses.html', context={"Courses" : c})
+    return render(request, 'courses.html', context={"Courses": c})
+
 
 @login_required()
 def courseView(request):
     if request.method != 'GET':
-         return courseList(request)
+        return courseList(request)
     c = request.GET.get('c', '')
     if not c:
-        courseList(request)
+        return courseList(request)
     try:
         modules = models.Module.objects.filter(course=c)
         years = models.Course.objects.get(name=c)
         notes = models.Note.objects.filter(module__in=modules)
-        return render(request, 'modules.html', context={"Modules" : modules, "Years" : range(years.years), "Notes" : notes})
+        return render(request, 'modules.html',
+                      context={"Modules": modules, "Years": range(years.years), "Notes": notes})
     except Exception, e:
+        return courseList(request)
+
+
+@login_required()
+def noteView(request):
+    if request.method != "GET":
         return courseList()
+    c = request.GET.get('id', '')
+    if not c:
+        return courseList(request)
+    try:
+        note = models.Note.objects.get(id=id)
+        return render(request, 'notes.html', context={"Note" : note})
+    except Exception, e:
+        return courseList(request)
